31435d29938ee966d03d46b24fb07144693564e1
Muhib Dulquer <m.dulquer.sumon@gmail.com>
2024-11-25 13:42:26 +0600

    implementing the logic of employee account & claim token function

--- programs/vesting/src/lib.rs
+++ programs/vesting/src/lib.rs
@@ -7,14 +7,41 @@ pub mod vesting {
     use super::*;
 
     pub fn create_vesting_account(ctx: Context<CreateVestingAccount>, company_name:String) -> Result<()> {
-        
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            mint: ctx.accounts.mint.key(),
+            treasury_token_account: ctx.accounts.treasury_token_account.key(),
+            company_name,
+            treasury_bump: ctx.bumps.treasury_token_account,
+            bump: ctx.bumps.vesting_account,
+        };
 
         Ok(())
     }
+    pub fn create_employee_account(ctx: Context<CreateEmployeeAccount>, start_time:i64, end_time:i64, total_amount:i64,cliff_time:i64) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            beneficiary: ctx.accounts.beneficiary.key(),
+            start_time,
+            end_time,
+            total_amount,
+            total_withdrawn:0,
+            cliff_time,
+            vesting_account: ctx.accounts.vesting_account.as_key(),
+            bump:ctx.bumps.employee_account,
+        };
+
+        Ok(())
+    }
+
+    pub fn claim_tokens(ctx:Context<ClaimTokens>, company_name:String) ->Result<()> {
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
-#[instruction(company_name)]
+#[instruction(company_name: String)]
 pub struct CreateVestingAccount<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -41,6 +68,70 @@ pub struct CreateVestingAccount<'info> {
 
 }
 
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub beneficiary: SystemAccount<'info>,
+
+    #[account(
+       has_one = owner,
+
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    #[account(
+        init,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        payer = owner,
+        seeds = [b"employee_vesting",beneficiary.key().as_ref() vesting_account.key().as_ref()],
+        bump
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+
+
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct ClaimTokens<'info> {
+    #[account(mut)]
+    pub beneficiary: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"employee_vesting",beneficiary.key().as_ref() vesting_account.key().as_ref()],
+        bump= employee_account.bump,
+        has_one = beneficiary,
+        has_one = vesting_account
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    #[account(
+        mut,
+        seeds = [company_name.as_ref()],
+        bump= vesting_account.bump,
+        has_one = treasury_token_account,
+        has_one = mint
+    )]
+    pub vesting_account: Account<'info, EmployeeAccount>,
+    pub mint :InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = beneficiary,
+        associated_token::mint = mint,
+        associated_token::authority = beneficiary,
+        associated_token::token_program = token_program,
+
+    )]
+    pub employee_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Inteface<'info, TokenInterface>,
+    pub associated_token_program :Program<'info,AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount {
@@ -51,4 +142,18 @@ pub struct VestingAccount {
     pub company_name:String,
     pub treasury_bump: u8,
     pub bump: u8,
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+     pub beneficiary: Pubkey,
+     pub start_time: i64,
+     pub end_time: i64,
+     pub cliff_time: i64,
+     pub vesting_account: Pubkey,
+     pub total_amount: u64,
+     pub total_withdrawn: u64,
+     pub bump: u8,
 }
